624bc39d74b2414933f5523b281e728102216b71
NirmalW <nirmalw@mitesp.com>
2023-03-22 16:19:49 +0530

    ex2 and ex3

--- src/Tasks.java
+++ src/Tasks.java
@@ -1,6 +1,11 @@
 public class Tasks {
     public static void main(String args[]) {
+        ex1();
+        ex2();
+        ex3();
+    }
 
+    public static void ex1(){
         String word = "asb sscc ddd idssdi eersss";
 
         String words[] = word.split(" "); //split String into several words and store them in words String array
@@ -36,6 +41,40 @@ public class Tasks {
             System.out.println(words[x] + " \t  " + max + "   \t" + words[x].equalsIgnoreCase(reverse[x]));
         }
     }
+
+    public static void ex2(){
+
+        int[] t = {14, 9, 18, 13, 1, 12,0,23,9,4,1,14,1,7,5};
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t.length; i++) {
+            if (t[i] == 0) {
+                sb.append(' ');
+            } else {
+                char c = (char) (t[i] + 96);
+                sb.append(c);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void ex3(){
+            String str1 = "addss 099";
+            String str2 = "22309 dfd";
+            String result = "";
+
+            for (int i = 0; i < str1.length(); i++) {
+
+                for (int j = 0; j < str2.length(); j++) {
+                    char ch = str1.charAt(i);
+                    if (str1.charAt(i) == str2.charAt(j) && result.indexOf(ch) == -1 && str1.charAt(i) != 32) {
+                        result += ch;
+                    }
+                }
+            }
+            System.out.println("Output : " + result);
+        }
 }
 
 
